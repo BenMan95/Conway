5a1d7606f3502f1145308c04ac38c8dc6c958c09
Benjamin Chu <chu00071@umn.edu>
2023-05-11 22:22:11 -0500

    Implemented options menu, minor bug fixes.

--- src/Application.java
+++ src/Application.java
@@ -1,17 +1,84 @@
 import java.awt.EventQueue;
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public class Application extends JFrame {
 
+    private Board board;
+
     public Application() {
-        add(new Board(35, 35));
-
         setResizable(false);
-        pack();
-
         setTitle("Conway's Game of Life");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        makeMenu();
+        setBoard(new Board(35, 35));
         setLocationRelativeTo(null);
+    }
+
+    private void makeMenu() {
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menu = new JMenu("Options");
+        JMenuItem menuItem;
+
+        menuItem = new JMenuItem("Set size");
+        menuItem.addActionListener(e -> {
+            JTextField rowField = new JTextField(5);
+            JTextField colField = new JTextField(5);
+
+            JPanel panel = new JPanel();
+            panel.add(new JLabel("rows:"));
+            panel.add(rowField);
+            panel.add(Box.createHorizontalStrut(15));
+            panel.add(new JLabel("cols:"));
+            panel.add(colField);
+
+            int result = JOptionPane.showConfirmDialog(null, panel, "Set size", JOptionPane.OK_CANCEL_OPTION);
+            if (result == JOptionPane.OK_OPTION) {
+                try {
+                    int rows = Integer.parseInt(rowField.getText());
+                    int cols = Integer.parseInt(rowField.getText());
+                    Board b = new Board(rows, cols);
+                    setBoard(b);
+                } catch (NumberFormatException ex) {
+                    JOptionPane.showMessageDialog(null, "Input must be integers!", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+        menu.add(menuItem);
+
+        menuItem = new JMenuItem("Fill");
+        menuItem.addActionListener(e -> {
+            JPanel panel = new JPanel();
+            panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+
+            JSlider slider = new JSlider();
+            slider.setMajorTickSpacing(25);
+            slider.setPaintTicks(true);
+            slider.setPaintLabels(true);
+            slider.setSnapToTicks(false);
+
+            panel.add(new JLabel("Select fill percentage:"));
+            panel.add(slider);
+
+            int result = JOptionPane.showConfirmDialog(null, panel, "Fill", JOptionPane.OK_CANCEL_OPTION);
+            if (result == JOptionPane.OK_OPTION) {
+                board.setPaused(true);
+                board.fillBoard((double) slider.getValue() / 100);
+                board.repaint();
+            }
+        });
+        menu.add(menuItem);
+
+        menuBar.add(menu);
+        this.setJMenuBar(menuBar);
+    }
+
+    public void setBoard(Board board) {
+        if (this.board != null)
+            remove(this.board);
+        add(board);
+        this.board = board;
+        pack();
     }
 
     public static void main(String[] args) {
--- src/Board.java
+++ src/Board.java
@@ -21,13 +21,10 @@ public class Board extends JPanel implements ActionListener {
         this.cols = cols;
         cells = new boolean[rows][cols];
 
-        Random rand = new Random();
-        for (int r = 0; r < rows; r++)
-            for (int c = 0; c < cols; c++)
-                cells[r][c] = rand.nextBoolean();
+        fillBoard(0);
 
         delay = 128;
-        timer = new Timer(125, this);
+        timer = new Timer(delay, this);
         timer.setInitialDelay(0);
         paused = true;
 
@@ -40,23 +37,15 @@ public class Board extends JPanel implements ActionListener {
         setBackground(Color.BLACK);
     }
 
-    @Override
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        Graphics2D g2d = (Graphics2D) g;
-        drawBoard(g2d);
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        updateBoard();
-        repaint();
+    public void fillBoard(double density) {
+        Random rand = new Random();
+        for (int r = 0; r < rows; r++)
+            for (int c = 0; c < cols; c++)
+                cells[r][c] = rand.nextDouble() < density;
     }
 
     private void drawBoard(Graphics2D g2d) {
         g2d.setColor(Color.WHITE);
-        g2d.setStroke(new BasicStroke(1));
-
         for (int r = 0; r < rows; r++) {
             int y = r * CELL_SIZE;
             for (int c = 0; c < cols; c++) {
@@ -91,6 +80,25 @@ public class Board extends JPanel implements ActionListener {
         return count;
     }
 
+    public void setPaused(boolean p) {
+        paused = p;
+        if (p) timer.stop();
+        else timer.start();
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+        drawBoard(g2d);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        updateBoard();
+        repaint();
+    }
+
     private class Mouse extends MouseAdapter {
         @Override
         public void mousePressed(MouseEvent e) {
@@ -98,10 +106,8 @@ public class Board extends JPanel implements ActionListener {
                 int c = e.getX() / CELL_SIZE;
                 int r = e.getY() / CELL_SIZE;
                 int button = e.getButton();
-                if (button == MouseEvent.BUTTON1)
-                    cells[r][c] = true;
-                if (button == MouseEvent.BUTTON3)
-                    cells[r][c] = false;
+                if (button == MouseEvent.BUTTON1) cells[r][c] = true;
+                if (button == MouseEvent.BUTTON3) cells[r][c] = false;
                 repaint();
             }
         }
@@ -111,17 +117,10 @@ public class Board extends JPanel implements ActionListener {
         @Override
         public void keyPressed(KeyEvent e) {
             char key = e.getKeyChar();
-            if (key == ' ') {
-                if (paused) {
-                    paused = false;
-                    timer.start();
-                } else {
-                    paused = true;
-                    timer.stop();
-                }
-            } else if (key == ',') {
-                if (delay > 1)
-                    delay <<= 1;
+            if (key == ' ') setPaused(!paused);
+            else if (key == ',') {
+                if (delay == 0) delay = 1;
+                else delay <<= 1;
                 timer.setDelay(delay);
             } else if (key == '.') {
                 delay >>= 1;
